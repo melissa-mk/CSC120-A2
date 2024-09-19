c7eaad72bd1922eae279e3438d19fd7bfa4a784a
Mugisha Melissa Kabalisa <mkabalisa@smith.edu>
2024-09-18 22:29:21 -0400

    added price and os update methods

--- computer.py
+++ computer.py
@@ -1,10 +1,23 @@
 class Computer:
 
     # What attributes will it need?
+    description:str
+    processor_type:str
+    hard_drive_capacity:int
+    memory:int
+    os:str
+    year_made:int
+    price:int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, os, year_made, price):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.os = os
+        self.year_made = year_made
+        self.price = price
 
     # What methods will you need?
